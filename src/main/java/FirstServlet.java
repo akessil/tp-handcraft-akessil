1fab26fb675d2dea8a7b2ac4d9c58088d8aaff81
ouikene koceila <koceila.ouikene@worldline.com>
2019-01-14 11:49:33 +0100

    part 4 delete web.xml

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,10 +1,12 @@
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/*", loadOnStartup = 1)
 public class FirstServlet extends HttpServlet {
 
     @Override
